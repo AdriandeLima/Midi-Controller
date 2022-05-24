3d4939118b5fd8ec184898848bf5aeab0f6e727c
adriandelima <adrianethandelima@gmail.com>
2022-05-24 09:50:40 +0100

    New Code, using integration

---
+++ Integrated midiwheel code/Integrated_midiwheel_code/Integrated_midiwheel_code.ino
@@ -0,0 +1,44 @@
+//There's a floating voltage somewhere, because I can't keep Integral at 0. Sometimes it get's stuck at e.g. -10, which then breaks
+//Midi integral, because that's mapped from 0 to 1023. Maybe this should change aswell.
+
+#include "MIDIUSB.h"
+    // Integrate motor voltage example
+int Integral;
+int aVal;
+int lastMidiIntegral;
+#define zeroOffset 344
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  aVal = (analogRead(A0) -zeroOffset);
+  Integral += aVal/4;
+  Serial.print(analogRead(A0) -zeroOffset); //What's this for?
+  Serial.print("  ");
+//  Serial.println(Integral);
+  delay(100);
+
+  int midiIntegral = map(Integral,0, 1023, 0, 127);
+//  constrain(midiIntegral, 0, 127);
+  if (midiIntegral>127){
+    midiIntegral = 127;
+  }
+  else if (midiIntegral <0){
+    midiIntegral =0;
+  }
+  
+  Serial.println(midiIntegral);
+  delay(100);
+  if(midiIntegral != lastMidiIntegral){
+  controlChange(2, 9, midiIntegral); //  (channel, CC number,  CC value) (slows down operation of wheel)
+  MidiUSB.flush();
+  }
+  lastMidiIntegral = midiIntegral;
+}
+
+void controlChange(byte channel, byte control, byte value) {
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  MidiUSB.sendMIDI(event);
+}
